298d5c672cca32ea918bd59259a9c30c6ac173f7
Leonardo <leonardomichalskim@gmail.com>
2023-09-12 16:39:54 -0300

    Add a script that runs a hardcoded command list

---
+++ ttyecho.py
@@ -0,0 +1,15 @@
+import os
+import termios
+from fcntl import ioctl
+
+# To find out the terminal devpts file, run "tty" command in it.
+DEV_PTS = "/dev/pts/1"
+CMD_LIST = ['ls\n']*20
+
+fd = os.open(DEV_PTS, os.O_RDWR)
+
+for cmd in CMD_LIST:
+    for c in cmd:
+        fcntl.ioctl(fd, termios.TIOCSTI, c)
+
+os.close(fd)
